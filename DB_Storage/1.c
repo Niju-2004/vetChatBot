bbd25b34ad8a8caa6d0a8780dd286994d8c7ca6f
Niju-2004 <145272536+Niju-2004@users.noreply.github.com>
2025-01-02 11:08:02 +0530

    Add files via upload

---
+++ DB_Storage/1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    float a=7.97, b=8;
+    printf("The value of a = %f and b = %f",a, b );
+    /*printf("Enter the first number \n");
+    scanf("%d", &a);
+    printf("Enter the second number \n");
+    scanf("%d", &b);
+    */
+    printf(" \n The value of a+b = %f", a+b);
+    printf("\n The value of a-b %f", a-b);
+    printf("\n The vaule of adfdj %f %f", a++, b++);
+    return 0;
+}
+
+
